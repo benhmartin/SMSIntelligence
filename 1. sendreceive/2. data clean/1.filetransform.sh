aca61f37008950c1371c33f25dac42362169ac89
benhmartin <100083038+benhmartin@users.noreply.github.com>
2022-02-23 14:23:38 -0500

    cleaned up my notes
    I.... say things when I'm documenting. I probably shouldn't do that in public.

--- 1. sendreceive/2. data clean/1.filetransform.sh
+++ 1. sendreceive/2. data clean/1.filetransform.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-#what we're doing with this script is using line one to remove the first line of text that has the headers, because this is a database and not a French beach.
-#line two converts every instance of more than one space into a tab. which should be unnecessary since this is a tabbed separated values file, but
-#you know. sometimes we just get fucked by things.
-#speaking of fucked, we're still trying to figure out how in the shit we're supposed to convert the twilio date format into something that mariadb can actually understand
-#since right now it's in RFC1123 format and we need it to be not that. 
+#what we're doing with this script is using line one to remove the first line
+#of text that has the headers, because this is a database and not a French beach.
+#line two converts every instance of more than one space into a tab. which
+#should be unnecessary since this is a tabbed separated values file, but
 sed -i '1d' sendreceive.tsv
 sed 's/ \+ /\t/g' sendreceive.tsv > sendreceivetabbed.tsv
